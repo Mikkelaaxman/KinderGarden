393a71564723ae3c542b4ebf36b04ed0c86d9ba8
emilnorsker <56673512+emilnorsker@users.noreply.github.com>
2020-03-29 21:16:37 +0200

    update on employees

--- src/main/java/project/kindergarden/controllers/ScheduleController.java
+++ src/main/java/project/kindergarden/controllers/ScheduleController.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import project.kindergarden.models.ScheduleModel;
 
 @RestController
 @RequestMapping(value = "/schedule")
 public class ScheduleController
 {
-
+    ScheduleModel scheduleModel;
     public ScheduleController()
     {
+        scheduleModel = new ScheduleModel();
     }
 
 
@@ -21,6 +23,12 @@ public class ScheduleController
         {
             ModelAndView mv = new ModelAndView();
             mv.setViewName("schedule");
+
+            mv.addObject("thisYear", scheduleModel.getCurrentYear());
+            mv.addObject("thisMonth",  scheduleModel.getCurrentMonth());
+            mv.addObject("thisDay",  scheduleModel.getCurrentDay());
+            mv.addObject("daysInMonth", scheduleModel.totalDaysInMonth());
+
 
 
 
