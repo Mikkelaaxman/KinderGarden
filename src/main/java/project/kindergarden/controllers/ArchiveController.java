e97185ac1c16c71f439705644aaa23081be064c5
emilnorsker <56673512+emilnorsker@users.noreply.github.com>
2020-04-02 20:14:23 +0200

    update on Schedule

--- src/main/java/project/kindergarden/controllers/ArchiveController.java
+++ src/main/java/project/kindergarden/controllers/ArchiveController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import project.kindergarden.data.Gender;
+import project.kindergarden.models.ChildModel;
 import project.kindergarden.models.ListUtil;
 
 import java.time.LocalDate;
@@ -16,60 +17,13 @@ import java.util.List;
 @RequestMapping(value = "/archive")
 public class ArchiveController
 {
-    ArrayList<test> list = new ArrayList<>();
+    ArrayList<ChildModel> list = new ArrayList<>();
     ModelAndView mv;
     ListUtil listUtil = new ListUtil();
 
     public ArchiveController()
     {
-        list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));
-        list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));list.add(new test(1,"emil1", "pretty good at coding"));
-        list.add(new test(2,"emil2", "pretty good at html"));
-        list.add(new test(3,"emil3", "pretty handsome"));
-        list.add(new test(4,"emil4", "pretty pretty"));
-        list.add(new test(5,"emil5", "pretty clever"));
-        list.add(new test(6,"emil6", "pretty good humor"));
-
+        list = (ArrayList<ChildModel>) listUtil.getChildList();
         listUtil.CreateChild(9999, new int[99], "THe", "Dude", Gender.M, LocalDate.parse("2020-05-22"), LocalDate.parse("1990-05-22"), LocalDate.parse("1990-05-22"), true, "Wheres my rug");
         System.out.println(listUtil.getChildList().get(0).getBirthDate() );
         listUtil.getChildList().get(0).setBirthDate(LocalDate.parse("1010-05-22"));
@@ -96,11 +50,8 @@ public class ArchiveController
         mv.setViewName("archive");
         mv.addObject("list", list);
 
-        System.out.println(mv.getModel().get("chosen"));
-        System.out.println("id -  "+ id);
-
         int i = Integer.parseInt(id);
-        mv.addObject("chosen", list.get(i-1)); // replace in model with a get by id
+        mv.addObject("chosen", listUtil.ReadChild(i)); // replace in model with a get by id
         return mv;
     }
 
--- src/main/java/project/kindergarden/models/data/DataInitiator.java
+++
@@ -1,9 +0,0 @@
-package project.kindergarden.models.data;
-
-public interface DataInitiator
-{
-    public void createElement(String id);
-    public void getElement(String id);
-    public void updateElement(String id);
-    public void deleteElement(String id);
-}
