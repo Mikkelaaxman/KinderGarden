e3233ff5599c85fe23ec34766b1f5ed98368e9b1
emilnorsker <56673512+emilnorsker@users.noreply.github.com>
2020-04-03 02:35:50 +0200

    update 5.2

--- src/main/java/project/kindergarden/models/repository/ChildRepository.java
+++ src/main/java/project/kindergarden/models/repository/ChildRepository.java
@@ -1,10 +1,12 @@
 package project.kindergarden.models.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import project.kindergarden.models.model.ChildModel;
 
 @Repository
-public interface ChildRepository
+public interface ChildRepository extends JpaRepository<ChildModel, Long>
 {
 
 }
